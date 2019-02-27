cb593517340c468ef08d30c59f326840b4925dcb
github username <github email address>
2019-02-27 18:03:28 -0500

    Done.

--- src/hog.js
+++ src/hog.js
@@ -21,24 +21,58 @@ class Hog {
     const hogImg = document.createElement('img')
     hogImg.src = this.image
     hogDiv.appendChild(hogImg)
-    // hogDiv.appendChild(this.greasedCheckbox())
+    hogDiv.appendChild(this.greasedCheckbox())
     return hogDiv
   }
 
   greasedCheckbox() {
-    
+    let checkdiv = document.createElement('div')
+    checkdiv.innerHTML = 'Greased: '
+    let checkbox = document.createElement('input')
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.checked = this.greased
+    checkbox.id = `check-${this.id}`
+    checkbox.addEventListener('click', () => {this.handleCheckbox(checkbox.checked)})
+    checkdiv.appendChild(checkbox)
+    return checkdiv
   }
 
-  static postHog(id, name, specialty, weight, highestMedal, image) {
-    fetch(Hog.api, {
-      method: post,
-      headers: {
+  handleCheckbox(checkboxBool) {
+    this.patchHog(checkboxBool)
+  }
 
+  patchHog(checkboxBool) {
+    fetch(Hog.api + `/${this.id}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json"
       },
-      body: {
-        
-      }
+      body: JSON.stringify({
+        "greased": checkboxBool
+      })
     })
+  }
+
+  static postHog(name, specialty, greased, weight, highestMedal, image) {
+    const hogCollection = document.getElementById('hog-container')
+    fetch(Hog.api, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "name": name,
+        "specialty": specialty,
+        "greased": greased,
+        "weight": weight,
+        "highest medal achieved": highestMedal,
+        "image": image
+      })
+    }).then(resp => resp.json())
+      .then(jsonHogulo => {
+        let pig = new Hog(jsonHogulo.id, jsonHogulo.name, jsonHogulo.specialty, jsonHogulo.greased, jsonHogulo.weight, jsonHogulo["highest medal achieved"], jsonHogulo.image)
+        hogCollection.appendChild(pig.createHogDiv())
+      })
   }
 
   static acquireHogs(callback) {
--- src/index.js
+++ src/index.js
@@ -13,15 +13,19 @@ function populateHogs() {
 }
 
 function activateForm() {
-    document.querySelector('form').addEventListener('submit', handleForm)
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        handleForm()
+    })
 }
 
 function handleForm() {
+    const check = document.querySelector("input[name='greased']").checked
     const name = document.querySelector("input[name='name']").value
     const specialty = document.querySelector("input[name='specialty']").value
     const medal = document.querySelector("input[name='medal']").value
     const weight = document.querySelector("input[name='weight']").value
     const image = document.querySelector("input[name='img']").value
-
+    Hog.postHog(name, specialty, check, medal, weight, image)
 }
 
