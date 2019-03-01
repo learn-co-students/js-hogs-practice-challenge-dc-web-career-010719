1be3aaed2172abc71d4073a2ef62753d904fa235
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-02-28 23:08:10 -0500

    refactored the code

--- src/index.js
+++ src/index.js
@@ -1,138 +1,159 @@
-let hogContainer
-let form
-
 document.addEventListener("DOMContentLoaded", init)
 
 function init() {
-  hogContainer = document.querySelector('#hog-container')
   getAllHogs()
 
-  form = document.querySelector('#hog-form')
-  form.addEventListener('submit', handleClickOfSubmit)
+  let hogForm = getHogForm()
+  hogForm.addEventListener('submit', handleSubmitOfForm)
 }
 
 function getAllHogs() {
   fetch('http://localhost:3000/hogs')
   .then(res => res.json())
-  .then(allHogsData => {
-    allHogsData.forEach(renderHog)
+  .then(allHogObjs => {
+    allHogObjs.forEach(renderHog)
   })
 }
 
 function renderHog(hogObj) {
-  let div = document.createElement('div')
-  hogContainer.appendChild(div)
-  div.classList.add('hog-card')
-  div.id = "hog-" + hogObj.id
+  let hogContainer = getHogContainer()
 
-  let img = document.createElement('img')
-  div.appendChild(img)
-  img.src = hogObj.image
+  let hogCard = document.createElement('div')
+  hogContainer.appendChild(hogCard)
+  hogCard.id = 'hog-card-' + hogObj.id
+  hogCard.classList.add('hog-card')
 
-  let h2 = document.createElement('h2')
-  div.appendChild(h2)
-  h2.innerText = hogObj.name
+  let image = document.createElement('img')
+  hogCard.appendChild(image)
+  image.src = hogObj.image
 
-  let checkbox = document.createElement('input')
-  div.appendChild(checkbox)
-  checkbox.type = "checkbox"
-  checkbox.dataset.id = hogObj.id
-  checkbox.id = "checkbox-" + hogObj.id
+  let nameHolder = document.createElement('h2')
+  hogCard.appendChild(nameHolder)
+  nameHolder.innerText = hogObj.name
+
+  let specialtyHolder = document.createElement('p')
+  hogCard.appendChild(specialtyHolder)
+  specialtyHolder.innerText = `Specialty: ${hogObj.specialty}`
+
+  let greasedLabel = document.createElement('label')
+  hogCard.appendChild(greasedLabel)
+  greasedLabel.for = 'greased-checkbox-' + hogObj.id
+  greasedLabel.innerText = "Greased:"
+  let greasedCheckbox = document.createElement('input')
+  hogCard.appendChild(greasedCheckbox)
+  greasedCheckbox.id = 'greased-checkbox-' + hogObj.id
+  greasedCheckbox.dataset.id = hogObj.id
+  greasedCheckbox.type = 'checkbox'
   if (hogObj.greased === true) {
-    checkbox.checked = true
+    greasedCheckbox.checked = true
   } else {
-    checkbox.checked = false
+    greasedCheckbox.checked = false
   }
-  let label = document.createElement('label')
-  div.appendChild(label)
-  label.for = `checkbox-${hogObj.id}`
-  label.innerText = "greased"
-  checkbox.addEventListener('change', handleChangeOfCheckbox)
+  greasedCheckbox.addEventListener('change', handleChangeOfGreasedCheckbox)
 
-  let p3= document.createElement('p')
-  div.appendChild(p3)
-  p3.innerText = `specialty: ${hogObj.specialty}`
+  let weightHolder = document.createElement('p')
+  hogCard.appendChild(weightHolder)
+  weightHolder.innerText = `Weight: ${hogObj["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"]}`
 
-  let p1 = document.createElement('p')
-  div.appendChild(p1)
-  p1.innerText = `weight: ${hogObj["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"]}`
+  let medalHolder = document.createElement('p')
+  hogCard.appendChild(medalHolder)
+  medalHolder.innerText = `Medal: ${hogObj["highest medal achieved"]}`
 
-  let p2 = document.createElement('p')
-  div.appendChild(p2)
-  p2.innerText = `medal: ${hogObj['highest medal achieved']}`
-
-  let deleteButton = document.createElement('button')
-  div.appendChild(deleteButton)
-  deleteButton.innerText = "Delete"
-  deleteButton.dataset.id = hogObj.id
-  deleteButton.addEventListener('click', handleClickOfDeleteButton)
+  let hogDeleteButton = document.createElement('button')
+  hogCard.appendChild(hogDeleteButton)
+  hogDeleteButton.innerText = "Delete"
+  hogDeleteButton.dataset.id = hogObj.id
+  hogDeleteButton.addEventListener('click', handleClickOfHogDeleteButton)
 }
 
-function handleClickOfSubmit(event) {
+function handleSubmitOfForm(event) {
   event.preventDefault()
   postNewHog()
 }
 
 function postNewHog() {
-  let postData = {
-    name: document.querySelectorAll('input')[0].value,
-    specialty: document.querySelectorAll('input')[1].value,
-    "highest medal achieved": document.querySelectorAll('input')[2].value,
-    "weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water": parseInt(document.querySelectorAll('input')[3].value),
-    image: document.querySelectorAll('input')[4].value,
-  }
-  if (document.querySelectorAll('input')[5].checked === true) {
-    postData.greased = true
-  } else {
-    postData.greased = false
-  }
-  document.querySelectorAll('input')[0].value = ''
-  document.querySelectorAll('input')[1].value = ''
-  document.querySelectorAll('input')[2].value = ''
-  document.querySelectorAll('input')[3].value = ''
-  document.querySelectorAll('input')[4].value = ''
-  document.querySelectorAll('input')[5].checked = false
   fetch('http://localhost:3000/hogs', {
     method: "POST",
-    body: JSON.stringify(postData),
+    body: JSON.stringify(getPostData()),
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     }
   }).then(res => res.json())
-    .then(newHogData => {
-      renderHog(newHogData)
-    })
+    .then(newHogObj => {renderHog(newHogObj)})
 }
 
-function handleClickOfDeleteButton(event) {
-  let id = event.currentTarget.dataset.id
-  fetch(`http://localhost:3000/hogs/${id}`, {
+function handleClickOfHogDeleteButton(event) {
+  let hogId = event.currentTarget.dataset.id
+  getHogCard(hogId).remove()
+  deleteHog(hogId)
+}
+
+function deleteHog(hogId) {
+  fetch(`http://localhost:3000/hogs/${hogId}`, {
     method: "DELETE"
-  }).then(res => res.json())
-    .then(() => {
-      document.querySelector(`#hog-${id}`).remove()
-    })
+  })
 }
 
-function handleChangeOfCheckbox(event) {
-  let id = event.currentTarget.dataset.id
-  let patchData
-  if (document.querySelector(`#checkbox-${id}`).checked === true) {
-    patchData = {
-      greased: true
-    }
-  } else {
-    patchData = {
-      greased: false
-    }
-  }
-  fetch(`http://localhost:3000/hogs/${id}`, {
+function handleChangeOfGreasedCheckbox(event) {
+  let hogId = event.currentTarget.dataset.id
+  patchGreasedOfHog(hogId)
+}
+
+function patchGreasedOfHog(hogId) {
+  fetch(`http://localhost:3000/hogs/${hogId}`, {
     method: "PATCH",
-    body: JSON.stringify(patchData),
+    body: JSON.stringify(getPatchDataForGreased(hogId)),
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     }
   })
+}
+
+//Helper Functions
+function getHogContainer() {
+  return document.querySelector('#hog-container')
+}
+
+function getHogForm() {
+  return document.querySelector("#hog-form")
+}
+
+function getPostData() {
+  let postData = {}
+  postData.name = getFormInputs()[0].value
+  postData.specialty = getFormInputs()[1].value
+  postData["highest medal achieved"] = getFormInputs()[2].value
+  postData["weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water"] = getFormInputs()[3].value
+  postData.image = getFormInputs()[4].value
+  if (getFormInputs()[5].checked === true) {
+    postData.greased = true
+  } else {
+    postData.greased = false
+  }
+  for (const index in getFormInputs()) {
+    if (index === 5) {
+      getFormInputs()[index].checked = false
+    } else if (index < 5) {
+      getFormInputs()[index].value = ''
+    }
+  }
+  return postData
+}
+
+function getFormInputs() {
+  return document.querySelectorAll("input")
+}
+
+function getHogCard(hogId) {
+  return document.querySelector(`#hog-card-${hogId}`)
+}
+
+function getPatchDataForGreased(hogId) {
+  if (document.querySelector(`#greased-checkbox-${hogId}`).checked === true) {
+    return {greased: true}
+  } else {
+    return {greased: false}
+  }
 }
